a29a73808bdde304fb4aa3411b585cb864d9ac60
Mat Ellis <mat@cloudability.com>
2013-08-03 17:05:41 -0700

    Move binding function so you can use it in your views

--- lib/axr/helpers.rb
+++ lib/axr/helpers.rb
@@ -29,23 +29,27 @@ module AjaxfulRating # :nodoc:
       %{<script>
         #{authenticity_script}
 
-        $(document).ready(function(){
+        function bindAllAxrElements() {
           $('.ajaxful-rating a').bind('click',function(event){
             event.preventDefault();
             $.ajax({
-              type: $(this).attr('data-method'),
-              url: $(this).attr('href'),
-              data: {
-                      stars: $(this).attr('data-stars'),
-                      dimension: $(this).attr('data-dimension'),
-                      size: $(this).attr('data-size'),
-                      show_user_rating: $(this).attr('data-show_user_rating')
-                    },
-              success: function(response){
-                $('#' + response.id + ' .show-value').css('width', response.width + '%');
-              }
+               type: $(this).attr('data-method'),
+               url: $(this).attr('data-url'),
+               data: {
+                       stars: $(this).attr('data-stars'),
+                       dimension: $(this).attr('data-dimension'),
+                       size: $(this).attr('data-size'),
+                       show_user_rating: $(this).attr('data-show_user_rating')
+                     },
+               success: function(response){
+                 $('#' + response.id + ' .show-value').css('width', response.width + '%');
+               }
             });
           });
+        }
+
+        $(document).ready(function(){
+          bindAllAxrElements();
         });
       </script>}
     end
